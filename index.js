f3e30856230f289e6ca77be2a95981d85a344a5d
Chris <sirchrissarnacki@gmail.com>
2023-10-03 10:35:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function longestWord(string) {
+    let str = string.split(' ');
+    let length = 0;
+    let word = null;
+
+    for (let i = 0; i < str.length; i++) {
+        if (length < str[i].length) {
+            length = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+
+console.log(longestWord('The weather is cold today'));
+console.log(longestWord('I am hungry'));
+console.log(longestWord('The car is brown'));
