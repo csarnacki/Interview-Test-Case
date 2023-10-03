0f9a3eb35438df69a804196ba1dfd0b3f6cae38d
Chris <sirchrissarnacki@gmail.com>
2023-10-03 10:40:54 -0700

    Added comments

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function longestWord(string) {
+    //Split the passed in string into individual sub strings
     let str = string.split(' ');
+    //Set initial length to 0
     let length = 0;
     let word = null;
 
+    //For loop used to run through the length of each sub string
     for (let i = 0; i < str.length; i++) {
+        //Check to see if length is less than each sub string length
         if (length < str[i].length) {
+            //Return it if this evaluates to true
             length = str[i].length;
+            //Set word variable equal to the returned sub string
             word = str[i];
         }
     }
